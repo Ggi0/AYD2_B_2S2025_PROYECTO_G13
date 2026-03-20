5a1e548855ec5601deda1098117276c62217d445
Giovanni Concohá <gconcohacax21@gmail.com>
2026-03-20 02:01:27 -0600

    202100229 : Principio del backend controlador de rutas establecido

--- FASE 2/backend/index.js
+++ FASE 2/backend/index.js
@@ -1,17 +1,37 @@
-// Instala primero express con: npm install express
+// Instala primero las dependencias con:
+// npm install express cors
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
 
-// Middleware para parsear JSON
+// Importar rutas principales
+const indexRoutes = require('./src/routes/index_routes');
+
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
 // Ruta básica
 app.get('/', (req, res) => {
-  res.send('Api funcionado');
+  res.status(200).json({
+    ok: true,
+    mensaje: 'API funcionando'
+  });
 });
 
+// Todas las rutas iniciarán con /api
+app.use('/api', indexRoutes);
+
+// Middleware para rutas no encontradas
+app.use((req, res) => {
+  res.status(404).json({
+    ok: false,
+    mensaje: 'Ruta no encontrada'
+  });
+});
 
 // Iniciar servidor
 app.listen(PORT, () => {
---
+++ FASE 2/backend/src/routes/index_routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+// Ruta --> api/
+router.get('/', (req, res) => {
+    res.status(200).json({
+      ok: true,
+      mensaje: 'Dentro de la API'
+    });
+  });
+
+// Importar rutas por módulo
+const facturacionRoutes = require('./facturacion/routes_factuacion');
+
+// otras carpetas por ejemplo:
+// const contratosRoutes = require('./contratos/routes_contratos');
+
+
+// Convención general:
+// /api/funcion
+router.use('/facturacion', facturacionRoutes);
+//  /api/contratos
+//  router.use('/contratos', contratosRoutes);
+
+// ... asi para todas las demas rutas
+
+module.exports = router;
